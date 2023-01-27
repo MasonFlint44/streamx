dded577c2c1ff5669f5e73253cb902b2e6e4c42a
Mason Flint <masonflint44@gmail.com>
2023-01-27 06:36:23 +0000

    added some tests around basic usage

--- streamz/stream.py
+++ streamz/stream.py
@@ -4,7 +4,8 @@ from typing import Generic, TypeVar
 
 T = TypeVar("T")
 
-
+# TODO: if stream is closed, should we raise an error when trying to push items?
+# TODO: if stream is closed, should we raise an error when trying to iterate over it?
 class AsyncStream(AsyncIterator[T], Generic[T], AsyncIterable[T]):
     def __init__(self) -> None:
         self._queue: asyncio.Queue[T] = asyncio.Queue()
@@ -13,7 +14,7 @@ class AsyncStream(AsyncIterator[T], Generic[T], AsyncIterable[T]):
     async def push(self, item: T) -> None:
         current_task = asyncio.current_task()
         if self._consuming_task == current_task:
-            raise ValueError("Can't push item while stream is being consumed by the same task")
+            raise RuntimeError("Can't push item while stream is being consumed by the same task")
         await self._queue.put(item)
 
     async def close(self) -> None:
@@ -21,7 +22,7 @@ class AsyncStream(AsyncIterator[T], Generic[T], AsyncIterable[T]):
 
     def __aiter__(self) -> "AsyncStream[T]":
         if self._consuming_task is not None:
-            raise ValueError("Stream is already being consumed by another task.")
+            raise RuntimeError("Stream is already being consumed by another task.")
         self._consuming_task = asyncio.current_task()
         return self
 
--- tests/test_stream.py
+++ tests/test_stream.py
@@ -1,2 +1,69 @@
-def test_stream():
-    pass
+import asyncio
+
+import pytest
+
+from streamz import AsyncStream
+
+
+@pytest.mark.asyncio
+async def test_push_and_consume():
+    stream: AsyncStream[int] = AsyncStream()
+
+    async def producer():
+        for i in range(10):
+            await stream.push(i)
+        await stream.close()
+
+    async def consumer():
+        items = []
+        async for item in stream:
+            items.append(item)
+        return items
+
+    asyncio.create_task(producer())
+    result = await consumer()
+    assert result == list(range(10))
+
+
+@pytest.mark.asyncio
+async def test_push_from_same_task_raises_error():
+    stream: AsyncStream[int] = AsyncStream()
+
+    async def producer():
+        for i in range(10):
+            await stream.push(i)
+        await stream.close()
+
+    async def consumer():
+        async for _ in stream:
+            with pytest.raises(RuntimeError):
+                await stream.push(123)
+
+    asyncio.create_task(producer())
+    consumer_task = asyncio.create_task(consumer())
+    await consumer_task
+
+
+@pytest.mark.asyncio
+async def test_multiple_consumers_raises_error():
+    stream = AsyncStream()
+
+    async def producer():
+        for i in range(10):
+            await stream.push(i)
+        await stream.close()
+
+    async def consumer1():
+        items = []
+        async for item in stream:
+            items.append(item)
+        return items
+
+    async def consumer2():
+        with pytest.raises(RuntimeError):
+            async for _ in stream:
+                pass
+
+    asyncio.create_task(producer())
+    asyncio.create_task(consumer1())
+    asyncio.create_task(consumer2())
